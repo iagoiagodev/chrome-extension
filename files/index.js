648c59612f2eba720a584650309d06c2e37459e1
iagoiago <iagolaferl1401@gmail.com>
2025-04-27 18:49:32 -0300

    first commit

---
+++ files/index.js
@@ -0,0 +1,8 @@
+const saveBtn = document.querySelector(`#save-btn`);
+const inputEl = document.querySelector(`#input-el`);
+
+let myLead = [];
+
+saveBtn.addEventListener('click', function () {
+  console.log('Save button clicked!');
+});
