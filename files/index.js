3eb44153755fb026ee71ebf048e96c92da94b032
iagoiago <iagolaferl1401@gmail.com>
2025-05-03 12:48:46 -0300

    Update

--- files/index.js
+++ files/index.js
@@ -1,8 +1,14 @@
 const saveBtn = document.querySelector(`#save-btn`);
 const inputEl = document.querySelector(`#input-el`);
+const ulEl = document.querySelector(`#ul-el`);
 
-let myLead = [];
+let myLeads = [`aaaaaaa`, `bbbbbbbbbb`, `ccccc`];
 
 saveBtn.addEventListener('click', function () {
-  console.log('Save button clicked!');
+  myLeads.push(inputEl.value);
+  console.log(myLeads);
 });
+
+for (let i = 0; i < myLeads.length; i++) {
+  ulEl.innerHTML += `<li>${myLeads[i]} </li`;
+}
